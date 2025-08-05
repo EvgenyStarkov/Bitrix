acb6d08db113164eea3a6fd02ae350fe99cad798
EvgenyStarkov <Estarkov135@gmail.com>
2025-08-05 20:29:06 +0300

    [ex1-1, ex1-2]

---
+++ partneram/raspisanie-miropriyatiy/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Анонсы", 
+		"/partneram/raspisanie-miropriyatiy/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Архив", 
+		"/partneram/raspisanie-miropriyatiy/arkhiv.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
